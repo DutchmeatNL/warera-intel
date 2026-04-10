49e36dfd6b2aeea1c47049ccd2d59d36cfd0eac1
Jeroen <yerouen@hotmail.com>
2026-04-10 14:16:27 +0200

    Third draft of political scanner

--- app/api/diplomacy/route.ts
+++ app/api/diplomacy/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  const NEDERLAND_ID = 'HIER_HET_ID_VAN_NL'; // Check of dit een string is, bijv: '10'
+  const NEDERLAND_ID = '6813b6d446e731854c7ac7a0'; // Check of dit een string is, bijv: '10'
   const API_KEY = process.env.WARERA_API_KEY;
 
   console.log("Systeem probeert verbinding te maken met ID:", NEDERLAND_ID);
